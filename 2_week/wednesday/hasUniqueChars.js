2bc8cc1f7ba92fc3983e3709ed590cac5b069c74
Angela Lee <angelalee@Angelas-iMac.local>
2023-06-28 09:27:58 -0500

    wednesday morning ex

---
+++ 2_week/wednesday/hasUniqueChars.js
@@ -0,0 +1,30 @@
+// Write your code below
+// approach 1
+/*const hasUniqueChars = (str) => {
+  let seen = new Set();
+  for (char of str) {
+    if (seen.has(char)) {
+      return false;
+    } else {
+      seen.add(char);
+    }
+  }
+  return true;
+};*/
+
+//approach 2
+const hasUniqueChars = (str) => {
+  let obj = {};
+  for (char of str) {
+    if (obj[char]) {
+      return false;
+    } else {
+      obj[char] = true;
+    }
+  }
+  return true;
+};
+console.log(hasUniqueChars("Monday"));
+console.log(hasUniqueChars("Moonday"));
+console.log(hasUniqueChars("Boob"));
+console.log(hasUniqueChars("Bob"));
