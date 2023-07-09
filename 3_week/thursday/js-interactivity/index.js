5030a0e72d768005563f60c78c37bb04b7cb2b6a
Angela Lee <angelalee@Angelas-iMac.local>
2023-07-09 16:15:04 -0500

    refractory some code

--- 3_week/thursday/js-interactivity/index.js
+++ 3_week/thursday/js-interactivity/index.js
@@ -1,48 +1,3 @@
-console.log("hello world");
-
-/*const message = document.querySelector("#message");
-
-const addMovie = (e) => {
-  e.preventDefault();
-  const inputField = document.querySelector("input");
-  const movie = document.createElement("li");
-  const movieTitle = document.createElement("span");
-  movieTitle.textContent = inputField.value;
-  movieTitle.addEventListener("click", crossOffMovie);
-  movie.appendChild(movieTitle);
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "X";
-  deleteBtn.addEventListener("click", deleteMovie);
-  movie.appendChild(deleteBtn);
-  const list = document.querySelector("ul");
-  list.appendChild(movie);
-  inputField.value = "";
-};
-
-document.querySelector("form").addEventListener("submit", addMovie);
-
-const deleteMovie = (e) => {
-  e.target.parentNode.remove();
-  message.textContent = "Movie deleted";
-  revealMessage();
-};
-
-const crossOffMovie = (e) => {
-  e.target.classList.toggle("checked");
-  if (e.target.classList.contains("checked")) {
-    message.textContent = `${e.target.textContent} watched!`;
-  } else {
-    message.textContent = `${e.target.textContent} added back!`;
-  }
-  revealMessage();
-};
-
-const revealMessage = (cb, time) => {
-  message.classList.remove("hide");
-  setTimeout(() => {
-    message.classList.add("hide");
-  }, 1000);
-};*/
 const message = document.querySelector("#message");
 const addMovie = (e) => {
   e.preventDefault();
@@ -60,17 +15,28 @@ const addMovie = (e) => {
   inputField.value = "";
 };
 
-const deleteMovie = (e) => {
+document.querySelector("form").addEventListener("submit", addMovie);
+
+deleteMovie = (e) => {
+  //message.textContent = "Movie deleted!";
+  message.textContent = `${e.target.parentNode.firstChild.textContent} deleted!`;
+  revealMessage();
   e.target.parentNode.remove();
-  message.textContent = "Movie deleted";
 };
 
 const crossOffMovie = (e) => {
   e.target.classList.toggle("checked");
   if (e.target.classList.contains("checked")) {
-    message.textContent = "Movie watched!";
+    // message.textContent = "Movie watched";
+    message.textContent = `${e.target.textContent} watched!`;
   } else {
-    message.textContent = "Movie added back";
+    //message.textContent = "Movie added back!";
+    message.textContent = `${e.target.textContent} added back!`;
   }
+  revealMessage();
 };
-document.querySelector("form").addEventListener("submit", addMovie);
+
+const revealMessage = () => {
+  setTimeout(() => message.classList.add("hide"), 1000);
+  message.classList.remove("hide");
+};
